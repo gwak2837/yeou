dcfc5e6b17528fead54c2bb9b3b337a346e28229
Taeuk Gwak <gwak2837@kakao.com>
2022-11-28 20:49:00 +0900

    Add crawler

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,25 @@ import { ReactNode } from 'react'
 import ChannelTalk from '../components/ChannelTalk'
 import GoogleAnalytics from '../components/GoogleAnalytics'
 
-const myFont = localFont({ src: './PretendardVariable.woff2' })
+const myFont = localFont({
+  src: './PretendardVariable.woff2',
+  fallback: [
+    'Pretendard',
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'system-ui',
+    'Roboto',
+    'Helvetica Neue',
+    'Segoe UI',
+    'Apple SD Gothic Neo',
+    'Noto Sans KR',
+    'Malgun Gothic',
+    'Apple Color Emoji',
+    'Segoe UI Emoji',
+    'Segoe UI Symbol',
+    'sans-serif',
+  ],
+})
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,48 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import { load } from 'cheerio'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import puppeteer from 'puppeteer-extra'
+import StealthPlugin from 'puppeteer-extra-plugin-stealth'
 
-type Data = {
-  name: string;
-};
+puppeteer.use(StealthPlugin())
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: "John Doe" });
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const productURL = req.query.productURL
+
+  if (!productURL || typeof productURL !== 'string')
+    return res.status(400).json({ message: 'Please input productURL' })
+
+  let response
+  try {
+    response = await fetch(productURL)
+  } catch (err) {
+    return res.status(400).json({ message: 'Failed fetching HTML' })
+  }
+
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto(productURL)
+  await page.waitForSelector('.prod-coupon-download-content', { timeout: 5000 })
+
+  const content = await page.content()
+
+  const $ = load(content)
+  // const $ = load(await response.text())
+
+  const price = $('.prod-sale-price > .total-price').text().trim()
+  const price2 = $('.prod-coupon-price > .total-price').text().trim()
+  const coupon = $('.prod-coupon-download-content').html()
+  const card = $('.benefit-label').text()
+  const imageUrl = $('.prod-image__detail').attr('src')
+  const reviewCount = $('#prod-review-nav-link > span.count').text()
+  const name = $('.prod-buy-header__title').text()
+
+  res.status(200).json({
+    name,
+    price,
+    price2,
+    coupon,
+    card,
+    imageUrl: `https:${imageUrl}`,
+    reviewCount,
+  })
 }
