dcb638e627ffae15e070a8860d9bd804d71fae40
Taeuk Gwak <gwak2837@kakao.com>
2023-01-17 20:36:10 +0900

    Delete Username.tsx

--- src/components/Username.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import useCurrentUser from '../hooks/useCurrentUser'
-
-export default function Username() {
-  const currentUser = useCurrentUser() as any
-
-  return <pre>{JSON.stringify(currentUser ?? ' ', null, 2)}</pre>
-}
